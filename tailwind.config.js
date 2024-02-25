a5a502650eade69592bbb5bec14b597e433680d0
Christian Ivan Cazarin de Anda <c.cazarin@outlook.com>
2024-02-24 20:27:01 -0600

    Paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'orange': 'hsl(26deg , 100%, 55%)',
+      'paleOrange': 'hsl(25deg , 100%, 94%)',
+      'veryDarkBlue': 'hsl(220deg , 13%, 13%)',
+      'darkGrayishBlue': 'hsl(219deg , 9%, 45%)',
+      'grayishBlue': 'hsl(220deg , 14%, 75%)',
+      'lightGrayishBlue': 'hsl(223deg , 64%, 98%)',
+
+    }
   },
   plugins: [],
 }
