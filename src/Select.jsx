09379e199f203e04e6a688b2bf41dc6a7adc1eb3
Christian Ivan Cazarin de Anda <c.cazarin@outlook.com>
2024-03-01 23:20:18 -0600

    Se agregaron estados a los botones de Select

--- src/Select.jsx
+++ src/Select.jsx
@@ -7,6 +7,10 @@ const Select = () => {
 
     const[items, setItems] = useState(0); 
 
+    const[show1, setShow1] = useState(true);
+    const[show2, setShow2] = useState(true);
+    const[show3, setShow3] = useState(true);
+
     return(
 
         <div className="w-full">
@@ -32,19 +36,22 @@ const Select = () => {
             </div>
             <div className="w-full flex gap-5">
                 <div className="flex">
-                    <button className="bg-lightGrayishBlue text-orange font-medium text-4xl px-4 py leading-none rounded-l-md" onClick={ () => setItems(items == 0 ? 0 : items - 1) }>
+                    <button className="bg-lightGrayishBlue text-orange font-medium text-4xl px-4 py leading-none rounded-l-md relative" onClick={ () => setItems(items == 0 ? 0 : items - 1) } onMouseOver={ () => setShow3( false ) } onMouseLeave={ () => setShow3( true ) } >
                         <Minus/>
+                        <div className='absolute top-0 left-0 bg-paleOrange bg-opacity-25 w-full h-full rounded-l-md' hidden={show3}></div>
                     </button>
                     <div className="bg-lightGrayishBlue px-3 flex justify-center items-center">
                         <p className="font-bold text-veryDarkBlue">{items}</p>
                     </div>
-                    <button className="bg-lightGrayishBlue text-orange font-medium text-4xl px-3 py-4 leading-none rounded-r-md" onClick={ () => setItems(items + 1) }>
+                    <button className="bg-lightGrayishBlue text-orange font-medium text-4xl px-3 py-4 leading-none rounded-r-md relative" onClick={ () => setItems(items + 1) } onMouseOver={ () => setShow2( false ) } onMouseLeave={ () => setShow2( true ) } >
                         <Plus/>
+                        <div className='absolute top-0 left-0 bg-paleOrange bg-opacity-25 w-full h-full rounded-r-md' hidden={show2}></div>
                     </button>
                 </div>
                 <div className='w-full'>
-                    <button className="bg-orange h-full rounded-md text-white font-semibold leading-3 w-full">
-                        <Carrito className="fill-white inline mr-3"/>Add to cart
+                    <button className="bg-orange h-full rounded-md text-white font-semibold leading-3 w-full relative" onMouseOver={ () => setShow1( false ) } onMouseLeave={ () => setShow1( true ) } >
+                        <><Carrito className="fill-white inline mr-3"/>Add to cart</>
+                        <div className='absolute top-0 bg-paleOrange bg-opacity-25 w-full h-full rounded-md' hidden={show1}></div>
                     </button>
                 </div>
             </div>
