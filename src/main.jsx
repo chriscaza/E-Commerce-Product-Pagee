9f54b036d23345d719f1ca4bddb029501d1d50fa
Christian Ivan Cazarin de Anda <c.cazarin@outlook.com>
2024-03-10 15:12:59 -0600

    Hook createContext para agregar los items al carrito

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Menu from './Menu'
 import Shop from  './Shop'
 
+//Create a new context and export
+export const CantidadContext = createContext();
+
+//Create a Context Provider
+const CantidadContextProvider = ({ children }) => {
+  const [cantidad, setCantidad] = useState(0);
+
+  return (
+    <CantidadContext.Provider value={{cantidad, setCantidad}}>
+      {children}
+    </CantidadContext.Provider>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Menu />
-    <Shop />
+    <CantidadContextProvider>
+      <Menu />
+      <Shop />
+    </CantidadContextProvider>
   </React.StrictMode>,
 )
